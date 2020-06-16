376b4eedbdd7e214a6514f6e7be644edf81a1320
Connor Kanalec <57271684+ConkyCode@users.noreply.github.com>
2020-06-15 20:09:45 -0400

    added the bounding box around faces

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./FaceRecognition.css"
 
-const FaceRecognition = ({imageUrl}) => {
+const FaceRecognition = ({imageUrl, box}) => {
 
     return (
         <div className="faceRecognitionContainer">
-            <img src={imageUrl} alt="" className="facePic"/>
+            <div className="wrapper">
+                <img id="inputImage" src={imageUrl} alt="" className="facePic"/>
+                <div className="boundingBox" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+            </div>
         </div>
     );
 
