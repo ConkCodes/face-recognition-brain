c8a12eacb7c1dd7bf3fdf5bf28d761f849336d7a
Connor Kanalec <57271684+ConkyCode@users.noreply.github.com>
2020-10-21 17:15:02 -0400

    Updates

---
+++ precache-manifest.56c80ac2f1c35e1a64a18b3aa21cf851.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f475f05aadf0bb08ef44255964a95c7",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "329ec72807dce337a1b2",
+    "url": "/face-recognition-brain/static/css/main.bb1914c5.chunk.css"
+  },
+  {
+    "revision": "f67ce3ca53e26b30320c",
+    "url": "/face-recognition-brain/static/js/2.725e16c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-recognition-brain/static/js/2.725e16c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "329ec72807dce337a1b2",
+    "url": "/face-recognition-brain/static/js/main.74f8b340.chunk.js"
+  },
+  {
+    "revision": "3c831b7d05b727d2a7c0",
+    "url": "/face-recognition-brain/static/js/runtime-main.e38ce823.js"
+  }
+]);
